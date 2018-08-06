94c61333f6b305e0e7fd58f1f1d6e0bfa7882083
天 谢 <jsjtxietian@outlook.com>
2018-08-06 16:13:34 +0800

    add error handling ; simple parser done

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -36,7 +36,7 @@ public class Lox {
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input);
 
-        for (;;) {
+        for (; ; ) {
             System.out.print("> ");
             run(reader.readLine());
             hadError = false;
@@ -44,13 +44,21 @@ public class Lox {
     }
 
     private static void run(String source) {
-         Scanner scanner = new Scanner(source);
-         List<Token> tokens = scanner.scanTokens();
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
 
-         // For now, just print the tokens.
-         for (Token token : tokens) {
-             System.out.println(token);
-         }
+//        // For now, just print the tokens.
+//        for (Token token : tokens) {
+//            System.out.println(token);
+//        }
+
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        // Stop if there was a syntax error.
+        if (hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
 
     }
 
@@ -61,5 +69,13 @@ public class Lox {
     private static void report(int line, String where, String message) {
         System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 }
