30634967c46d41399e585cbb0a623987b4e1e8c2
天 谢 <jsjtxietian@outlook.com>
2018-08-09 14:50:10 +0800

    add interpreter

--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -5,6 +5,8 @@ import java.util.List;
 import static com.craftinginterpreters.lox.TokenType.*;
 
 //expression → equality ;
+// change to (comma exp) =>  expression → equality (,equality)* ; //todo add ternary
+
 //equality → comparison ( ( "!=" | "==" ) comparison )* ;
 //comparison → addition ( ( ">" | ">=" | "<" | "<=" ) addition )* ;
 //addition → multiplication ( ( "-" | "+" ) multiplication )* ;
@@ -32,7 +34,15 @@ class Parser {
     }
 
     private Expr expression() {
-        return equality();
+        Expr expr = equality();
+
+        while(match(COMMA)){
+            Token operator = previous();
+            Expr right = equality();
+            expr = new Expr.Binary(expr,operator,right);
+        }
+
+        return expr;
     }
 
     private Expr equality() {
