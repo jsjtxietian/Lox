6877b2205349b27c47b71f3e650cf2318889ac8a
天 谢 <jsjtxietian@outlook.com>
2018-09-20 14:13:13 +0800

    astprinter modified , maybe to much

--- src/com/craftinginterpreters/lox/AstPrinter.java
+++ src/com/craftinginterpreters/lox/AstPrinter.java
@@ -1,7 +1,9 @@
 //package com.craftinginterpreters.lox;
 //
+//import java.util.List;
+//
 //// Creates an unambiguous, if ugly, string representation of AST nodes.
-//class AstPrinter implements Expr.Visitor<String> {
+//class AstPrinter implements Expr.Visitor<String> , Stmt.Visitor<String>  {
 //    String print(Expr expr) {
 //        return expr.accept(this);
 //    }
@@ -12,8 +14,27 @@
 //    }
 //
 //    @Override
-//    public String visitAssignExpr(Expr.Assign expr){
-//        return parenthesize("assign" , expr);
+//    public String visitThisExpr(Expr.This expr) {
+//        return expr.keyword.toString();
+//    }
+//
+//    @Override
+//    public String visitBlockStmt(Stmt.Block stmt) {
+//        return parenthesize("Block",stmt.statements);
+//    }
+//
+//    @Override
+//    public String visitBreakStmt(Stmt.Break stmt){
+//        return stmt.keyword.toString();
+//    }
+//
+//    @Override
+//    public String visitVarStmt(Stmt.Var stmt) {
+//        if (stmt.initializer != null) {
+//            return parenthesize("Var " + stmt.name.lexeme,stmt.initializer);
+//        }
+//
+//        return parenthesize("Var " + stmt.name.lexeme);
 //    }
 //
 //    @Override
@@ -49,6 +70,33 @@
 //
 //        return builder.toString();
 //    }
+//
+//    private String parenthesize(String name, Stmt... stmts) {
+//        StringBuilder builder = new StringBuilder();
+//
+//        builder.append("(").append(name);
+//        for (Stmt s : stmts) {
+//            builder.append(" ");
+//            builder.append(s.accept(this));
+//        }
+//        builder.append(")");
+//
+//        return builder.toString();
+//    }
+//
+//    private String parenthesize(String name, List<Stmt> statements) {
+//        StringBuilder builder = new StringBuilder();
+//
+//        builder.append("(").append(name);
+//        for (Stmt s : statements) {
+//            builder.append(" ");
+//            builder.append(s.accept(this));
+//        }
+//        builder.append(")");
+//
+//        return builder.toString();
+//    }
+//
 //
 //    //test
 //    public static void main(String[] args) {
